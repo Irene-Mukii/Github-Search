c7b2235151c6848038ab5213578d1c0e20bd5a38
renniequin@gmail.com <renniequin@gmail.com>
2021-12-07 16:15:24 +0300

    adds the http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
 import { GhContentDisplayedComponent } from './gh-content-displayed/gh-content-displayed.component';
 import { MyGhpageComponent } from './my-ghpage/my-ghpage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubSearchComponent,
     GhContentDisplayedComponent,
-    MyGhpageComponent
+    MyGhpageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
