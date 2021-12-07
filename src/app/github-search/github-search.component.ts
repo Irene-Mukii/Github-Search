c87f1f3f54ce4aacbcf275b1278cb49fd3ddcc92
renniequin@gmail.com <renniequin@gmail.com>
2021-12-07 16:00:06 +0300

    makes the http request to the api

--- src/app/github-search/github-search.component.ts
+++ src/app/github-search/github-search.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-github-search',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-search.component.css']
 })
 export class GithubSearchComponent implements OnInit {
+  repository!: Repo;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    interface ApiResponse{
+      name:string;
+      description:string;
+    }
+
+    this.http.get<ApiResponse>('https://api.github.com/users/daneden?access_token=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').subscribe((data: { name: any; description: any; }) => {
+      // Succesful API request
+      this.repository = new Repo()
+    })
   }
 
 }
