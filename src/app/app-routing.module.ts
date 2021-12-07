f386485c937df07addcf62092bcf9b0b9ef823b8
renniequin@gmail.com <renniequin@gmail.com>
2021-12-07 16:05:44 +0300

    adds routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubSearchComponent } from './github-search/github-search.component';
+import { MyGhpageComponent } from './my-ghpage/my-ghpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'github-search', component: GithubSearchComponent},
+  {path: 'my-ghpage', component: MyGhpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
